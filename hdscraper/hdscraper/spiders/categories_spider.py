387716b376a0fe648b648b470ddeb03ce4de4836
Ken Watson <watsonlimited05@gmail.com>
2023-03-08 10:22:33 -0800

    Updated the category spider and uploaded the json output.
    - Found that I was making a syntax error in the rules statement.

--- hdscraper/hdscraper/spiders/categories_spider.py
+++ hdscraper/hdscraper/spiders/categories_spider.py
@@ -1,18 +1,38 @@
+"""
+Scrapy crawl spider class that goes to the site map page
+and extracts all category names and links.
+"""
+
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 
-class CategoriesSpiderSpider(CrawlSpider):
-    name = "categories_spider"
+class CategoryCrawler(CrawlSpider):
+    """
+    Scrapy crawler class that goes to the site map page and extracts all
+    category links.
+    """
+
+    name = "category_crawler"
     allowed_domains = ["homedepot.com"]
     start_urls = ["https://www.homedepot.com/c/site_map"]
 
-    rules = (Rule(LinkExtractor(allow=r"Items/"), callback="parse_item", follow=True),)
+    # category_link_extractor = LinkExtractor()
+    # rule_category_link_extractor = Rule(
+    #     category_link_extractor, callback="parse_category", follow=False
+    # )
+    rules = (Rule(LinkExtractor(allow=("/b/",)), callback="parse_category"),)
 
-    def parse_item(self, response):
-        item = {}
-        # item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
-        # item["name"] = response.xpath('//div[@id="name"]').get()
-        # item["description"] = response.xpath('//div[@id="description"]').get()
-        return item
+    def parse_category(self, response):
+        """Parse category links."""
+        category_links = response.css("a[href*='/b/']::attr(href)").getall()
+        for category_link in category_links:
+            yield {
+                "category": response.css('a[href="' + category_link + '"]::text').get(),
+                "url": category_link,
+            }
+        # yield {
+        #     "category": response.css("p > a::text").get(),
+        #     "url": response.css("p > a::attr(href)").get(),
+        # }
