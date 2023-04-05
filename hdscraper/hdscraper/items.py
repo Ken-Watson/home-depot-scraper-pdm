19a2e7040b95b6409cc457ea36d7fdfe20f4ec47
Ken Watson <watsonlimited05@gmail.com>
2023-04-05 08:12:08 -0700

    - Added absolute file path to categories_spiders.py sqlite3 connection - Removed extra spaces in categories_spider.py

--- hdscraper/hdscraper/items.py
+++ hdscraper/hdscraper/items.py
@@ -7,11 +7,14 @@ import scrapy
 
 
 class HdscraperCategoryItem(scrapy.Item):
+    """Category item."""
+
     category = scrapy.Field()
     url = scrapy.Field()
 
 
 class HdscraperProductItems(scrapy.Item):
+    """Product item."""
     name = scrapy.Field()
     sku = scrapy.Field()
     price = scrapy.Field()
