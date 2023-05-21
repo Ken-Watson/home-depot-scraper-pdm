ac1d7896f885f8d1418be9c4af0d7a4b1a5a5a89
Ken Watson <watsonlimited05@gmail.com>
2023-05-21 14:40:47 -0700

    Added base database class with category database and product detail as separate classes

---
+++ hdscraper/database/product_detail_database.py
@@ -0,0 +1,16 @@
+"""This module is used to handle operations for the product details table."""
+
+from base_database import BaseDatabase
+
+class ProductDetailDatabase(BaseDatabase):
+    """This class is used to handle operations for the product details table."""
+
+    def create_table(self):
+        """Create the product details table"""
+        # Implement the SQL query to create the product details table.
+        pass
+
+    def write_data(self, data):
+        """Write the product details to the database."""
+        # Implement the SQL query to insert product details.
+        pass
