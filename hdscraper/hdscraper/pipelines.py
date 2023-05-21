686d71d255417369265df0fa2959d81cfd5d0524
Ken Watson <watsonlimited05@gmail.com>
2023-05-21 14:42:32 -0700

    Updated Scrapy pipelines file to reflect additions to database connections.

--- hdscraper/hdscraper/pipelines.py
+++ hdscraper/hdscraper/pipelines.py
@@ -5,57 +5,58 @@
 
 
 import sqlite3
+from typing import Optional
 
+from database.category_database import CategoryDatabase
+from database.product_detail_database import ProductDetailDatabase
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
-class SqliteCategoryPipeline:
-    """Pipeline for storing scraped items in the SQLite database."""
-
+class CategoryDatabasePipeline:
+    """Pipeline to handle the categories table."""
     def __init__(self):
-        self.conn = None
-        self.cursor = None
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        """Used to instantiate the pipeline object with any crawler-specific settings."""
-        return cls()
+        self.database: Optional[CategoryDatabase] = None
 
     def open_spider(self, spider):
         """Open the connection to the database."""
-        self.conn = sqlite3.connect(
-            r"c:\Users\watso\OneDrive\DataProjects\Git Repos\home-depot-scraper-pdm\hdscraper\hdscraper\hdscraper.db"
-        )
-        self.cursor = self.conn.cursor()
-        self.create_table()
+        self.database = CategoryDatabase("hdscraper.db")
+        self.database.create_table()
 
     def close_spider(self, spider):
         """Close the connection to the database."""
-        self.conn.close()
-
-    def create_table(self):
-        """Create the table if it doesn't exist."""
-        self.cursor.execute(
-            """CREATE TABLE IF NOT EXISTS categories (
-            id INTEGER PRIMARY KEY,
-            category TEXT,
-            url TEXT
-            );"""
-        )
-        self.conn.commit()
+        if self.database is not None:
+            self.database.close()
 
     def process_item(self, item, spider):
-        """Run the store_db method to store the item in the database."""
-        self.store_db(item)
-        return item
-
-    def store_db(self, item):
         """Write the categories and links to the database."""
-        category_title = item[
-            "category"
-        ].title()  # Capitalize each word in the category
-        self.cursor.execute(
-            """INSERT INTO categories (category, url) VALUES (?, ?)""",
-            (category_title, item["url"]),
-        )
-        self.conn.commit()
+        if item.get("category") and item.get("url"):
+            if self.database is not None:
+                self.database.write_data(item)
+            return item
+        else:
+            raise DropItem(f"Missing category or url in {item}")
+
+class CategoryDatabasePipeline:
+    """Pipeline to handle the categories table."""
+    def __init__(self):
+        self.database: Optional[ProductDetailDatabase] = None
+
+    def open_spider(self, spider):
+        """Open the connection to the database."""
+        self.database = ProductDetailDatabase("hdscraper.db")
+        self.database.create_table()
+
+    def close_spider(self, spider):
+        """Close the connection to the database."""
+        if self.database is not None:
+            self.database.close()
+
+    def process_item(self, item, spider):
+        """Write the categories and links to the database."""
+        if item.get("category") and item.get("url"):
+            if self.database is not None:
+                self.database.write_data(item)
+            return item
+        else:
+            raise DropItem(f"Missing category or url in {item}")
